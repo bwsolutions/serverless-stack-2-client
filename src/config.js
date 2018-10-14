ea5196cd26a8d18c67bf60cc1e58c205ad1670b3
Bill Stoltz <bill.stoltz@boosterwebsolutions.com>
2018-10-14 00:12:16 -0400

    update for mono-repo

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_QizypGeMoezZk1jnRUcJeZgj",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1w8twxh57odb1"
+    BUCKET: "notes-app-mono-uploads-dev-s3bucket-112p8wmmt8a0ncd"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://uqlien7vub.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://dro9b4uyjd.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_8uPZvWNTi",
-    APP_CLIENT_ID: "176k27ktpjb7b4md1s1rifneei",
-    IDENTITY_POOL_ID: "us-east-1:9c80f142-cb02-40ed-9b49-593104e4158d"
+    USER_POOL_ID: "us-east-1_NYD9GpXnu",
+    APP_CLIENT_ID: "25d8r1rbjpv68qpkmfad1p50k7",
+    IDENTITY_POOL_ID: "us-east-1:ef86816c-8398-417f-88fd-f4f4afb3bc53"
   }
 };
 
